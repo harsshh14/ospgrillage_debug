a1b828d0e4ec3b94491eb5c6e8defbe6500faba7
harsshh14 <81824273+harsshh14@users.noreply.github.com>
2025-01-12 23:09:35 +0530

    Update osp_grillage.py

--- src/ospgrillage/osp_grillage.py
+++ src/ospgrillage/osp_grillage.py
@@ -316,12 +316,12 @@ class OspGrillage:
 
         # edge support type
         self.edge_support_type_dict = {
-            edge_num: self.fixity_vector["fixed"]
+            edge_num: self.fixity_vector["pin"]
             for i, edge_num in enumerate(
                 list(set(self.Mesh_obj.edge_node_recorder.values()))
             )
         }
-        self.edge_support_type_dict.update({0: self.fixity_vector["pin"]})
+        self.edge_support_type_dict.update({0: self.fixity_vector["roller"]})
 
     def _create_mesh(self, **kwargs):
         """
