339d828bc9122a3fea435485298d17810e138f67
harsshh14 <81824273+harsshh14@users.noreply.github.com>
2025-01-15 11:35:57 +0530

    Update osp_grillage.py

--- src/ospgrillage/osp_grillage.py
+++ src/ospgrillage/osp_grillage.py
@@ -201,7 +201,7 @@ class OspGrillage:
         self.section_dict = {}  # of section tags
         self.material_dict = {}  # of material tags
         # variables related to analysis - which can be unique to element/material/ types
-        self.constraint_type = "Plain"  # base class - plain
+        self.constraint_type = "Transformation"  # base class - plain
         # collect mesh groups
         self.mesh_group = []  # for future release
         if self.mesh_type == "Ortho":
@@ -2475,7 +2475,7 @@ class Analysis:
         self.time_series_counter = time_series_counter
         self.plain_counter = pattern_counter
         # variables from keyword args
-        self.constraint_type = kwargs.get("constraint_type", "Plain")  # Default plain
+        self.constraint_type = kwargs.get("constraint_type", "Transformation")  # Default plain
         # Variables recording results of analysis
         self.node_disp = dict()  # key node tag, val list of dof
         self.ele_force = (
@@ -2490,7 +2490,7 @@ class Analysis:
         )  # ditto for global ele force except only for shells
         # preset ops analysis commands
         self.wipe_command = "ops.wipeAnalysis()\n"  # default wipe command
-        self.numberer_command = "ops.numberer('Plain')\n"  # default numberer is Plain
+        self.numberer_command = "ops.numberer('RCM')\n"  # default numberer is Plain
         self.system_command = "ops.system('BandGeneral')\n"  # default band general
         self.constraint_command = 'ops.constraints("{type}")\n'.format(
             type=self.constraint_type
@@ -2498,7 +2498,7 @@ class Analysis:
         self.algorithm_command = "ops.algorithm('Newton')\n"  # default linear
         self.analyze_command = "ops.analyze({})\n".format(self.step)  # default 1 step
         self.analysis_command = 'ops.analysis("{}")\n'.format(analysis_type)
-        self.intergrator_command = "ops.integrator('LoadControl', 1)\n"
+        self.intergrator_command = "ops.integrator('LoadControl', 0.5)\n"
         self.sensitivity_integrator_command = "ops."
         self.mesh_node_counter = node_counter  # set node counter based on current Mesh
         self.mesh_ele_counter = ele_counter  # set ele counter based on current Mesh
