a6d6e2f619ebb301909a2fa294e755f66c40c805
harsshh14 <81824273+harsshh14@users.noreply.github.com>
2025-04-06 12:36:34 +0530

    looking at ele and node cmd

--- src/ospgrillage/osp_grillage.py
+++ src/ospgrillage/osp_grillage.py
@@ -2614,7 +2614,7 @@ class OspGrillage:
             node_coord = self.Mesh_obj.node_spec[node_tag]["coordinate"]
             x_group = self.Mesh_obj.node_spec[node_tag]["x_group"]
             z_group = self.Mesh_obj.node_spec[node_tag]["z_group"]
-
+            
             # Create new coincident node
             new_node_tag = max(list(self.Mesh_obj.node_spec.keys())) + 1
             self.Mesh_obj.node_spec[new_node_tag] = {
@@ -2627,6 +2627,8 @@ class OspGrillage:
 
             # Create node command
             node_cmd = f"ops.node({new_node_tag}, {node_coord[0]}, {node_coord[1]}, {node_coord[2]})\n"
+            print("node_cmd")
+            print(node_cmd)
             if self.pyfile:
                 with open(self.filename, "a") as file_handle:
                     file_handle.write(node_cmd)
@@ -2638,6 +2640,8 @@ class OspGrillage:
             ele_cmd = (f"ops.element('zeroLength', {ele_tag_start}, {node_tag}, {new_node_tag}, "
                       f"'-mat', 1, 2, 3, 4, 5, 6, "
                       f"'-dir', 1, 2, 3, 4, 5, 6)\n")
+            print("ele_cmd")
+            print(ele_cmd)
             if self.pyfile:
                 with open(self.filename, "a") as file_handle:
                     file_handle.write(ele_cmd)
